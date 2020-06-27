aa07ec6c2e43af0bb0581ed2b234a5f11bc7929c
akclifto <akclifto@asu.edu>
2020-06-27 11:06:19 -0700

    adding queries and comments

--- VGS_Team2_select.sql
+++ VGS_Team2_select.sql
@@ -1,18 +1,21 @@
-/* *** PROJECT TEAM 2 DELIVERABLE 3 INSERT STATEMENTS *** 
--- Adam Clifton (akclifto@asu.edu)
--- Anne Landrum (aelandru@asu.edu)
--- Ivan Fernandez (iafernan@asu.edu)
--- Robert Ibarra (rnibarra@asu.edu)
+/* *** PROJECT TEAM 2 DELIVERABLE 3 SELECT QUERY STATEMENTS *** 
+	Adam Clifton (akclifto@asu.edu)
+	Anne Landrum (aelandru@asu.edu)
+	Ivan Fernandez (iafernan@asu.edu)
+	Robert Ibarra (rnibarra@asu.edu)
 */
+
 
 -- Query to select all platforms available on VGS by name.
 SELECT PLATFORM.Platform_Name
 FROM PLATFORM, PLATFORM_TYPE
 WHERE PLATFORM.Platform_Name = PLATFORM_TYPE.Platform_Name;
 
+
 -- Query to select all available titles, their description, ESRB Rating, and platform available on VGS
 SELECT TITLE.Title_Name, TITLE.Description, TITLE.ESRB_Rating, PLATFORM.Platform_Name
 FROM TITLE, PLATFORM;
+
 
 -- Query to select all current account members by member_id, full name, DOB that displays credit card number
 -- subscription activity, account email and platofrms used.
@@ -21,6 +24,7 @@ SELECT ACCOUNT.Member_ID, CUSTOMER.First_Name, CUSTOMER.Last_Name, CUSTOMER.Birt
 FROM ACCOUNT, PLATFORM, CUSTOMER
 WHERE ACCOUNT.Member_ID = CUSTOMER.Member_ID
 ORDER BY ACCOUNT.Member_ID ASC;
+
 
 -- Query to select games played by users in their user library.  Retrieved using UPC, Member_ID, Date Added, 
 -- and Date Removed from library. Further, selection provides information about the title including name, description,
@@ -31,9 +35,13 @@ SELECT USER_LIBRARY.UPC, USER_LIBRARY.Member_ID, USER_LIBRARY.Date_Added, USER_L
 FROM USER_LIBRARY, PLATFORM, TITLE 
 WHERE USER_LIBRARY.UPC = TITLE.UPC AND PLATFORM.UPC = TITLE.UPC;
 
+
 --  Query to select all titles added to user-library for user "TheCrow"
 SELECT acct.Member_ID, t.Title_Name
 FROM TITLE t
 	JOIN USER_LIBRARY ul ON ul.UPC = t.UPC
 	JOIN ACCOUNT acct ON acct.Member_ID = ul.Member_ID
 WHERE acct.Member_ID = 'TheCrow';
+
+
+-- 
