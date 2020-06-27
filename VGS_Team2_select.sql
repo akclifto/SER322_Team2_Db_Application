0bcacd95cfbc9da5ac752c07855172d1bb055f7f
akclifto <akclifto@asu.edu>
2020-06-27 11:24:10 -0700

    adding query

--- VGS_Team2_select.sql
+++ VGS_Team2_select.sql
@@ -14,7 +14,8 @@ WHERE PLATFORM.Platform_Name = PLATFORM_TYPE.Platform_Name;
 
 -- Query to select all available titles, their description, ESRB Rating, and platform available on VGS
 SELECT TITLE.Title_Name, TITLE.Description, TITLE.ESRB_Rating, PLATFORM.Platform_Name
-FROM TITLE, PLATFORM;
+FROM TITLE, PLATFORM
+ORDER BY TITLE.Title_Name ASC;
 
 
 -- Query to select all current account members by member_id, full name, DOB that displays credit card number
@@ -36,7 +37,7 @@ FROM USER_LIBRARY, PLATFORM, TITLE
 WHERE USER_LIBRARY.UPC = TITLE.UPC AND PLATFORM.UPC = TITLE.UPC;
 
 
---  Query to select all titles added to user-library for user "TheCrow"
+--  Query to select all titles added to user-library for user "TheCrow".
 SELECT acct.Member_ID, t.Title_Name
 FROM TITLE t
 	JOIN USER_LIBRARY ul ON ul.UPC = t.UPC
@@ -44,4 +45,13 @@ FROM TITLE t
 WHERE acct.Member_ID = 'TheCrow';
 
 
--- 
+-- Query to Select all saved game files for user "TheDestroyer85" that are actively in ther user's library and show game platform
+SELECT acct.Member_ID, t.Title_Name, p.Platform_Name
+FROM  TITLE t
+	JOIN USER_LIBRARY ul ON ul.UPC = t.UPC 
+	JOIN SAVE_GAME_FILE sgf ON sgf.UPC = ul.UPC
+	JOIN PLATFORM p ON p.Serial_Number = sgf.Serial_Number 
+	JOIN ACCOUNT acct ON acct.Member_ID = ul.Member_ID
+WHERE acct.Member_ID = "TheDestroyer85" 
+	AND t.UPC = sgf.UPC
+GROUP BY t.Title_Name;
