d5868e9acd449240cdf4d5466643c97b7095ad97
akclifto <akclifto@asu.edu>
2020-06-29 09:50:19 -0700

    init jdbc app

---
+++ src/vgs/Application.java
@@ -0,0 +1,21 @@
+package vgs;
+
+
+/**
+ * This application for Video Game Services (VGS) will be used as an interface to display a user menu that will
+ * load the VGS database and allow the user to run various related actions on the given database, implemented with
+ * JDBC.
+ *
+ * @author Adam Clifton (akclifto@asu.edu)
+ * @author Anne Landrum (aelandru@asu.edu)
+ * @author Ivan Fernandez (iafernan@asu.edu)
+ * @author Robert Ibarra (rnibarra@asu.edu)
+ *
+ * SER 322 Project Team 2 Deliverable 4
+ * @version 2020.06.29
+ * */
+public class Application {
+
+    //TODO:
+
+}
