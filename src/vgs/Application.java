e8e94ada8010cbf77e48e839a93cc3e59e77cfa5
akclifto <akclifto@asu.edu>
2020-06-30 17:42:24 -0700

    build main skeleton

--- src/vgs/Application.java
+++ src/vgs/Application.java
@@ -6,12 +6,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 /**
  * This application for Video Game Services (VGS) will be used as an interface to display a user menu that will
  * load the VGS database and allow the user to run various related actions on the given database, implemented with
  * JDBC.
  * <p>
+ *
  * @author Adam Clifton (akclifto@asu.edu)
  * @author Anne Landrum (aelandru@asu.edu)
  * @author Ivan Fernandez (iafernan@asu.edu)
@@ -27,6 +29,109 @@ public class Application {
     private static Statement stmt = null;
     private static Connection conn = null;
 
+    /**
+     * Method to run manual queries on database.
+     *
+     * @param scan : scanner for user input.
+     * @return void.
+     */
+    private static void writeQuery(Scanner scan) {
+        //TODO: manual queries
+    }
+
+    /**
+     * Method to run select.sql presets
+     *
+     * @param scan : scanner for user input.
+     * @return void.
+     */
+    private static void selectPreset(Scanner scan) {
+        //TODO:
+    }
+
+    /**
+     * Method to insert into database
+     *
+     * @param scan : scanner for user input.
+     * @return void.
+     */
+    private static void insertData(Scanner scan) {
+        //TODO:
+    }
+
+    /**
+     * Method to remove data from database
+     *
+     * @param scan : scanner for user input.
+     * @return void.
+     */
+    private static void removeData(Scanner scan) {
+        //TODO:
+    }
+
+
+    /**
+     * Method to exit program
+     *
+     * @param scan : scanner to close
+     * @return void.
+     */
+    private static void exitProgram(Scanner scan) {
+
+        System.out.println("Exiting Program.");
+
+
+    }
+
+
+    private static void getUserInput() {
+
+        System.out.println("Main Menu: ");
+        System.out.println("1 - Write your own query");
+        System.out.println("2 - Use preset queries provided with application"); //TODO: use default _select.sql
+        System.out.println("3 - Insert data into database");
+        System.out.println("4 - Remove data from database"); //TODO:  maybe do this one?
+        System.out.println("Q - Exit Program\n");
+        System.out.print("Please select an option: ");
+
+
+        try (Scanner scan = new Scanner(System.in)) {
+
+            // TODO: user inputs, then send off to either other class or methods.
+            while (scan.hasNext()) {
+
+                String in = scan.next();
+
+                if (in.equalsIgnoreCase("1")) {
+
+                    writeQuery(scan);
+                } else if (in.equals("2")) {
+
+                    selectPreset(scan);
+                } else if (in.equals(("3"))) {
+
+                    insertData(scan);
+                } else if (in.equals(("4"))) {
+
+                    removeData(scan);
+                } else if (in.equalsIgnoreCase(("Q"))) {
+
+                    exitProgram(scan);
+                } else {
+
+                    System.out.println("Invalid user input.  Please choose a valid option");
+                    getUserInput();
+                }
+            }
+
+
+            // TODO: should be in a while loop or similar while user input != "exit program"
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+        }
+    }
 
     /**
      * Entry point of the program.
@@ -59,23 +164,11 @@ public class Application {
         }
         System.out.println("done!\n");
 
-
-        System.out.println("Main Menu: ");
-        System.out.println("1 - Write your own query");
-        System.out.println("2 - Use preset queries provided with application"); //TODO: use default _select.sql
-        System.out.println("3 - Insert data into database");
-        System.out.println("4 - Remove data from database"); //TODO:  maybe do this one?
-        System.out.println("Q - Exit Program\n");
-        System.out.print("Please select an option: ");
-
-
+        // show menu and get user input
         try {
-
-            // TODO: user inputs, then send off to either other class or methods.
-            // TODO: should be in a while loop or similar while user input != "exit program"
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            getUserInput();
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             try {
                 if (rs != null) {
