8292441e37dd66ea162d08fbf58d2abe04536fe9
iafernan <66854846+iafernan@users.noreply.github.com>
2020-07-01 05:14:33 -0700

    Add files via upload
    Added methods writeQuery and selectPreset.

--- src/vgs/Application.java
+++ src/vgs/Application.java
@@ -1,4 +1,3 @@
-package vgs;
 
 
 import java.sql.Connection;
@@ -36,11 +35,147 @@ public class Application {
      * @return void.
      */
     private static void writeQuery(Scanner scan) {
-        //TODO: manual queries
-        System.out.println("placeholder writeQuery");
+        //manual queries
+        String query;
+        int selection;
 
-        //after method finished, get user input
-        getUserInput();
+        do {
+            System.out.println("Please enter the integer associated with the table that you want to query:");
+            System.out.println("1. Genre, 2. Title, 3. Platform, 4. Platform Type, 5. Account, 6. Customer, 7. User_Library");
+            System.out.println("8. Save_Game_File, 9. Logs_Into");
+            while (!scan.hasNextInt()) {
+                System.out.println("That's not a number!");
+                scan.next();
+            }
+            selection = scan.nextInt();
+        }while(selection < 1 || selection > 9);
+        System.out.println("Selection was successful!");
+
+        System.out.println("Please enter a SQL Query: ");
+        // get user input
+        query = scan.nextLine();
+
+        try {
+            // Create a statement
+            stmt = conn.createStatement();
+
+            // Setup a query
+            rs = stmt.executeQuery(query);
+
+            // Display the results
+            switch (selection) {
+                case 1:
+                    while (rs.next()) {
+                        System.out.print(rs.getString(1) + "\t");
+                        System.out.println(rs.getString(2));
+                    }
+                    break;
+
+                case 2:
+                    while (rs.next()) {
+                        System.out.print(rs.getLong(1) + "\t");
+                        System.out.print(rs.getString(2) + "\t");
+                        System.out.print(rs.getString(3) + "\t");
+                        System.out.print(rs.getInt(4) + "\t");
+                        System.out.print(rs.getString(5) + "\t");
+                        System.out.print(rs.getDouble(6) + "\t");
+                        System.out.print(rs.getString(7) + "\t");
+                        System.out.println(rs.getString(8));
+
+                    }
+                    break;
+
+                case 3:
+                    while (rs.next()) {
+                        System.out.print(rs.getLong(1) + "\t");
+                        System.out.print(rs.getLong(2)+ "\t");
+                        System.out.println(rs.getString(3));
+                    }
+                    break;
+
+                case 4:
+                    while (rs.next()) {
+                        System.out.print(rs.getString(1) + "\t");
+                        System.out.println(rs.getLong(2));
+                    }
+                    break;
+
+                case 5:
+                    while (rs.next()) {
+                        System.out.print(rs.getString(1) + "\t");
+                        System.out.print(rs.getString(2) + "\t");
+                        System.out.print(rs.getLong(3)+ "\t");
+                        System.out.print(rs.getBoolean(4) + "\t");
+                        System.out.print(rs.getString(5) + "\t");
+                        System.out.println(rs.getLong(6));
+
+                    }
+                    break;
+
+                case 6:
+                    while (rs.next()) {
+                        System.out.print(rs.getString(1) + "\t");
+                        System.out.print(rs.getString(2) + "\t");
+                        System.out.print(rs.getString(3) + "\t");
+                        System.out.print(rs.getString(4) + "\t");
+                        System.out.print(rs.getDate(5) + "\t");
+                        System.out.print(rs.getString(6) + "\t");
+                        System.out.println(rs.getString(7));
+
+                    }
+                    break;
+
+                case 7:
+                    while (rs.next()) {
+                        System.out.print(rs.getLong(1) + "\t");
+                        System.out.print(rs.getString(2) + "\t");
+                        System.out.print(rs.getString(3) + "\t");
+                        System.out.print(rs.getDate(4) + "\t");
+                        System.out.println(rs.getDate(5));
+
+                    }
+                    break;
+
+                case 8:
+                    while (rs.next()) {
+                        System.out.print(rs.getLong(1) + "\t");
+                        System.out.print(rs.getString(2) + "\t");
+                        System.out.print(rs.getString(3) + "\t");
+                        System.out.print(rs.getTime(4) + "\t");
+                        System.out.print(rs.getTime(5) + "\t");
+                        System.out.println(rs.getLong(6));
+
+                    }
+                    break;
+
+                case 9:
+                    while (rs.next()) {
+                        System.out.print(rs.getString(1) + "\t");
+                        System.out.print(rs.getString(2) + "\t");
+                        System.out.println(rs.getString(3));
+
+                    }
+                    break;
+
+            }
+        }
+        catch (Exception exc)
+        {
+            exc.printStackTrace();
+        }
+        finally {  // ALWAYS clean up DB resources
+            try {
+                if (rs != null)
+                    rs.close();
+                if (stmt != null)
+                    stmt.close();
+                if (conn != null)
+                    conn.close();
+            }
+            catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
     }
 
     /**
@@ -50,11 +185,160 @@ public class Application {
      * @return void.
      */
     private static void selectPreset(Scanner scan) {
-        //TODO: select presets
-        System.out.println("placeholder selectPreset");
+        //select presets
 
-        //after method finished, get user input
-        getUserInput();
+        String query1 ="SELECT PLATFORM.Platform_Name FROM PLATFORM, PLATFORM_TYPE WHERE PLATFORM.Platform_Name = PLATFORM_TYPE.Platform_Name";
+
+        String query2 = "SELECT distinct TITLE.Title_Name, TITLE.Description, TITLE.ESRB_Rating, PLATFORM.Platform_Name" +
+                      "FROM TITLE, PLATFORM ORDER BY TITLE.Title_Name ASC";
+
+        String query3 = "SELECT ACCOUNT.Member_ID, CUSTOMER.First_Name, CUSTOMER.Last_Name, CUSTOMER.Birthdate, " +
+                        "ACCOUNT.Credit_Card_Number, ACCOUNT.Subscription, ACCOUNT.Email, PLATFORM.Platform_Name" +
+                        "FROM ACCOUNT, PLATFORM, CUSTOMER WHERE ACCOUNT.Member_ID = CUSTOMER.Member_ID ORDER BY ACCOUNT.Member_ID ASC";
+
+        String query4 ="SELECT USER_LIBRARY.UPC, USER_LIBRARY.Member_ID, USER_LIBRARY.Date_Added, USER_LIBRARY.Date_Removed, " +
+                        "TITLE.Title_Name, TITLE.Description, TITLE.Year_Of_Release, TITLE.ESRB_Rating, TITLE.Metacritic_Rating, " +
+                        "TITLE.Genre_Name, TITLE.Genre_Description, PLATFORM.Platform_Name FROM USER_LIBRARY, PLATFORM, TITLE " +
+                        "WHERE USER_LIBRARY.UPC = TITLE.UPC AND PLATFORM.UPC = TITLE.UPC";
+
+        String query5 ="SELECT acct.Member_ID, t.Title_Name FROM TITLE t JOIN USER_LIBRARY ul ON ul.UPC = t.UPC " +
+                       "JOIN ACCOUNT acct ON acct.Member_ID = ul.Member_ID WHERE acct.Member_ID = 'TheCrow'";
+
+        String query6 ="SELECT acct.Member_ID, t.Title_Name, p.Platform_Name FROM  TITLE t JOIN USER_LIBRARY ul ON ul.UPC = t.UPC " +
+                       "JOIN SAVE_GAME_FILE sgf ON sgf.UPC = ul.UPC JOIN PLATFORM p ON p.Serial_Number = sgf.Serial_Number  " +
+                        "JOIN ACCOUNT acct ON acct.Member_ID = ul.Member_ID WHERE acct.Member_ID = 'TheDestroyer85' AND t.UPC = sgf.UPC " +
+                        "GROUP BY t.Title_Name, p.Platform_Name";
+
+        int selection;
+
+        do {
+        System.out.println("Please enter the integer associated with a preset Query");
+        System.out.println("1. " + query1);
+        System.out.println("2. " + query2);
+        System.out.println("3. " + query3);
+        System.out.println("4. " + query4);
+        System.out.println("5. " + query5);
+        System.out.println("6. " + query6);
+        //get user input and validate selection
+            while (!scan.hasNextInt()) {
+                System.out.println("That's not a number!");
+                scan.next();
+            }
+            selection = scan.nextInt();
+        }while(selection < 1 || selection > 6);
+        System.out.println("Selection was successful!");
+
+        try {
+            // Create a statement
+            stmt = conn.createStatement();
+
+
+            // Display the results
+            switch (selection) {
+                case 1:
+
+                    // Setup a query
+                    rs = stmt.executeQuery(query1);
+
+                    while (rs.next()) {
+                        System.out.print(rs.getString(1) + "\t");
+
+                    }
+                    break;
+
+                case 2:
+
+                    // Setup a query
+                    rs = stmt.executeQuery(query2);
+
+                    while (rs.next()) {
+                        System.out.print(rs.getString(1) + "\t");
+                        System.out.print(rs.getString(2) + "\t");
+                        System.out.print(rs.getString(3) + "\t");
+                        System.out.print(rs.getString(4) + "\t");
+
+
+                    }
+                    break;
+
+                case 3:
+                    // Setup a query
+                    rs = stmt.executeQuery(query3);
+
+                    while (rs.next()) {
+                        System.out.print(rs.getString(1) + "\t");
+                        System.out.print(rs.getString(2)+ "\t");
+                        System.out.print(rs.getString(3)+ "\t");
+                        System.out.print(rs.getDate(4)+ "\t");
+                        System.out.print(rs.getLong(5)+ "\t");
+                        System.out.print(rs.getBoolean(6)+ "\t");
+                        System.out.print(rs.getString(7)+ "\t");
+                        System.out.print(rs.getString(8)+ "\t");
+                    }
+                    break;
+
+                case 4:
+                    // Setup a query
+                    rs = stmt.executeQuery(query4);
+                    while (rs.next()) {
+                        System.out.print(rs.getLong(1) + "\t");
+                        System.out.print(rs.getString(2) + "\t");
+                        System.out.print(rs.getDate(3)+ "\t");
+                        System.out.print(rs.getDate(4)+ "\t");
+                        System.out.print(rs.getString(5) + "\t");
+                        System.out.print(rs.getString(6) + "\t");
+                        System.out.print(rs.getDate(7)+ "\t");
+                        System.out.print(rs.getString(8) + "\t");
+                        System.out.print(rs.getDouble(9) + "\t");
+                        System.out.print(rs.getString(10) + "\t");
+                        System.out.print(rs.getString(11) + "\t");
+                        System.out.print(rs.getString(12) + "\t");
+                    }
+                    break;
+
+                case 5:
+                    // Setup a query
+                    rs = stmt.executeQuery(query5);
+                    while (rs.next()) {
+                        System.out.print(rs.getString(1) + "\t");
+                        System.out.print(rs.getString(2) + "\t");
+
+                    }
+                    break;
+
+                case 6:
+                    // Setup a query
+                    rs = stmt.executeQuery(query6);
+                    while (rs.next()) {
+                        System.out.print(rs.getString(1) + "\t");
+                        System.out.print(rs.getString(2) + "\t");
+                        System.out.print(rs.getString(3) + "\t");
+
+                    }
+                    break;
+
+            }
+        }
+        catch (Exception exc)
+        {
+            exc.printStackTrace();
+        }
+        finally {  // ALWAYS clean up DB resources
+            try {
+                if (rs != null)
+                    rs.close();
+                if (stmt != null)
+                    stmt.close();
+                if (conn != null)
+                    conn.close();
+            }
+            catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+
+
+
     }
 
     /**
@@ -184,7 +468,7 @@ public class Application {
             getUserInput();
         } catch (Exception e) {
             e.printStackTrace();
-        // close everything related to sql server
+            // close everything related to sql server
         } finally {
             try {
                 if (rs != null) {
@@ -211,3 +495,5 @@ public class Application {
         }
     }
 }
+
+
