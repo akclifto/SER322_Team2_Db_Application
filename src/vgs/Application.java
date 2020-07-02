47d59652341844b8e163cce0f35f070f99b44226
akclifto <akclifto@asu.edu>
2020-07-01 22:16:42 -0700

    update comments

--- src/vgs/Application.java
+++ src/vgs/Application.java
@@ -12,14 +12,13 @@ import java.util.Scanner;
  * load the VGS database and allow the user to run various related actions on the given database, implemented with
  * JDBC.
  * <p>
- *
  * @author Adam Clifton (akclifto@asu.edu)
  * @author Anne Landrum (aelandru@asu.edu)
  * @author Ivan Fernandez (iafernan@asu.edu)
  * @author Robert Ibarra (rnibarra@asu.edu)
  * <p>
  * SER 322 Project Team 2 Deliverable 4
- * @version 2020.07-01
+ * @version 2020.07.01
  */
 public class Application {
 
